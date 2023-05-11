5e76087999d1e5de82d737dab9893eabb51c8494
Dustin Gouner <dustingouner20@gmail.com>
2023-05-11 15:39:41 -0600

    feat: Add header and input form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from "react"
+import Form from "./Form"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: []
+    }
+  }
+
+  render() {
+    return (
+    <div>
+      <header>
+        <p>🍌</p>
+        <h2 className='headerOne'>OVERRIPE</h2>
+        <h2 className='headerTwo'>BANANAS</h2>
+        <p>🍌</p>
       </header>
+      <Form />
     </div>
-  );
+    // <Movies />
+
+    )
+  }
 }
 
 export default App;
