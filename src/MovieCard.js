540ebd326a4dcdc5a0b2e6851f65cb0febd2eed7
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-05-11 18:32:23 -0700

    refact: add classnames for styling card

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -3,13 +3,13 @@ import './MovieCard.css'
 
 const MovieCard = ({poster, title, release, rating}) => {
   return (
-    <section className='movie'>
-      <img src={poster} alt='movie poster'/>
-      <h2 className='movie-title'>{title}</h2>
-      <p>Release Date: {release}</p>
-      <p>Rating: 🍌 {rating}</p>
+    <section className="movie">
+      <img src={poster} alt="movie poster" />
+      <h2 className="movie-title">{title}</h2>
+      <p className="movie-release">Release Date: {release}</p>
+      <p className="movie-rating">Rating: {rating} 🍌 </p>
     </section>
-  )
+  );
 }
 
 export default MovieCard
