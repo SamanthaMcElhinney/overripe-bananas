7ebcc405cfae824002d6fcb0c4ec10df3ac4ccf1
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-05-12 14:32:13 -0700

    Update movie class for spacing

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -3,12 +3,20 @@ import './MovieCard.css'
 
 const MovieCard = ({poster, title, release, rating}) => {
   return (
-    <section className="movie">
-      <img src={poster} alt="movie poster" />
-      <h2 className="movie-title">{title}</h2>
-      <p className="movie-release">Release Date: {release}</p>
-      <p className="movie-rating">Rating: {rating} 🍌 </p>
-    </section>
+    <div className="movie">
+      <img className="poster-image" src={poster} alt="movie poster" />
+      <div className="movie-card-info">
+        <span className="movie-title">{title}</span>
+        <span className="movie-release">Release Date: {release}</span>
+        <span className="movie-rating">Rating: {rating} 🍌 </span>
+      </div>
+    </div>
+    // <section className="movie">
+    //   <img src={poster} alt="movie poster" />
+    //   <h2 className="movie-title">{title}</h2>
+    //   <p className="movie-release">Release Date: {release}</p>
+    //   <p className="movie-rating">Rating: {rating} 🍌 </p>
+    // </section>
   );
 }
 
