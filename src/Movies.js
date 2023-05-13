50e7cca265eb0d0d99291c2af36d5bd4cb1e3d7c
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-05-12 18:44:20 -0700

    feat: passed movie info for individual movie into movie card

--- src/Movies.js
+++ src/Movies.js
@@ -2,26 +2,22 @@ import React from 'react'
 import MovieCard from './MovieCard'
 import './Movies.css'
 
-function Movies({movies}) {
-
-  const movieCards = movies.map(movie => {
+function Movies({ movies, getMovieInfo }) {
+  const movieCards = movies.map((movie) => {
     return (
-      <MovieCard 
+      <MovieCard
         id={movie.id}
         key={movie.id}
         poster={movie.poster_path}
         title={movie.title}
         release={movie.release_date}
         rating={movie.average_rating}
+        getMovieInfo={getMovieInfo}
       />
-    )
-  })
+    );
+  });
 
-  return (
-    <div className='movies-container'>
-      {movieCards}
-    </div>
-  )
+  return <div className="movies-container">{movieCards}</div>;
 }
 
 
