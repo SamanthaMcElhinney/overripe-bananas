3cf8fa07086f5f15e537989bb50e2914f0969a05
Dustin Gouner <dustingouner20@gmail.com>
2023-05-15 15:59:02 -0600

    style: style individual movie page

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -8,22 +8,6 @@ import './MovieDetails.css'
 
 function MovieDetails({ individualMovie }) {
   return (
-      // <section className="movie-details"
-        // style={{backgroundImage: `url(${individualMovie.movie.backdrop_path})`}}>
-      //   <div className="ind-card" style={{opacity:'1.0'}}>
-      //     <img className="ind-poster" src={individualMovie.movie.poster_path} />
-      //   </div>
-      //   <section className="movie-info">
-      //     <h2 className="movie-info-title">OVERVIEW</h2>
-      //     <p className="movie-overview">{individualMovie.movie.overview}</p>
-
-      //     <ul className="detail-list">
-      //       <li>Revenue: ${individualMovie.movie.revenue.toLocaleString("en-US")}</li>
-      //       <li>Budget: ${individualMovie.movie.budget.toLocaleString("en-US")}</li>
-      //       <li>Runtime: {individualMovie.movie.runtime} minutes</li>
-      //     </ul>
-      //   </section>
-      // </section>
       <div className="movie-container">
         <div className="overlay"
           style={{backgroundImage: `url(${individualMovie.movie.backdrop_path})`}}></div>
@@ -33,17 +17,16 @@ function MovieDetails({ individualMovie }) {
               <img className="ind-poster" src={individualMovie.movie.poster_path} alt="movie-poster"/>
             </div>
             <section className="movie-info">
-              <h2 className="movie-info-title">OVERVIEW</h2>
+              <h2 className="movie-info-title">{individualMovie.movie.title}</h2>
+              <h3 className="movie-tagline">"{individualMovie.movie.tagline}"</h3>
               <p className="movie-overview">{individualMovie.movie.overview}</p>
-
-              <ul className="detail-list">
-                <li>Revenue: ${individualMovie.movie.revenue.toLocaleString("en-US")}</li>
-                <li>Budget: ${individualMovie.movie.budget.toLocaleString("en-US")}</li>
-                <li>Runtime: {individualMovie.movie.runtime} minutes</li>
-              </ul>
+              <section className="stats">
+                <h3>Revenue: ${individualMovie.movie.revenue.toLocaleString("en-US")}</h3>
+                <h3>Budget: ${individualMovie.movie.budget.toLocaleString("en-US")}</h3>
+                <h3>Runtime: {individualMovie.movie.runtime} minutes</h3>
+              </section>
             </section>
           </div>
-
         </div>
       </div>
   );
