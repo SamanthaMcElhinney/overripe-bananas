2b2292dc322fd3908729958405783f410f37cff3
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-05-16 23:14:12 -0700

    Add header component

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = 
+
+<header>
+  <img className="headerImage" src={banana} alt="logo of a banana waving" />
+  <h1 className="headerOne">OVERRIPE</h1>
+  <h1 className="headerTwo">BANANAS</h1>
+</header>;
+
+
