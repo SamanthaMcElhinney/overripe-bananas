65a4e4b5cc0d224e59ad685e876d21c8154688b0
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-05-21 00:22:14 -0700

    refact: conditional rendering for movies without taglines

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -3,33 +3,56 @@ import PropTypes from 'prop-types'
 import './MovieDetails.css'
 
 function MovieDetails({ individualMovie, movieTrailer}) {
+  const tagline = individualMovie.movie.tagline
 
   return (
-      <div className="movie-container">
-        <div className="overlay"
-          style={{backgroundImage: `url(${individualMovie.movie.backdrop_path})`}}></div>
-        <div className="opacity">
-          <div className="content">
-            <div className="ind-card">
-              <img className="ind-poster" src={individualMovie.movie.poster_path} alt={`${individualMovie.movie.title} movie-poster`}/>
-            </div>
-            <section className="movie-info">
-              <h2 className="movie-info-title">{individualMovie.movie.title}</h2>
-              <h3 className="movie-tagline">"{individualMovie.movie.tagline}"</h3>
-              <p className="movie-overview">{individualMovie.movie.overview}</p>
-              <section className="stats">
-                <h3 className="stats-title">Movie Stats</h3>
-                <p className="revenue">Revenue: ${individualMovie.movie.revenue.toLocaleString("en-US")}</p>
-                <p className="budget">Budget: ${individualMovie.movie.budget.toLocaleString("en-US")}</p>
-                <p className="runtime">Runtime: {individualMovie.movie.runtime} minutes</p>
-              </section>
-            <div className="trailer-container">
-              <iframe className="trailer" src={`https://www.youtube.com/embed/${movieTrailer.key}`} title="Black Adam - Official Trailer 2" border="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </div>
-            </section>
+    <div className="movie-container">
+      <div
+        className="overlay"
+        style={{
+          backgroundImage: `url(${individualMovie.movie.backdrop_path})`,
+        }}
+      ></div>
+      <div className="opacity">
+        <div className="content">
+          <div className="ind-card">
+            <img
+              className="ind-poster"
+              src={individualMovie.movie.poster_path}
+              alt={`${individualMovie.movie.title} movie-poster`}
+            />
           </div>
+          <section className="movie-info">
+            <h2 className="movie-info-title">{individualMovie.movie.title}</h2>
+            {tagline && <h3 className="movie-tagline">"{tagline}"</h3>}
+            <p className="movie-overview">{individualMovie.movie.overview}</p>
+            <section className="stats">
+              <h3 className="stats-title">Movie Stats</h3>
+              <p className="revenue">
+                Revenue: $
+                {individualMovie.movie.revenue.toLocaleString("en-US")}
+              </p>
+              <p className="budget">
+                Budget: ${individualMovie.movie.budget.toLocaleString("en-US")}
+              </p>
+              <p className="runtime">
+                Runtime: {individualMovie.movie.runtime} minutes
+              </p>
+            </section>
+            <div className="trailer-container">
+              <iframe
+                className="trailer"
+                src={`https://www.youtube.com/embed/${movieTrailer.key}`}
+                title="Black Adam - Official Trailer 2"
+                border="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </section>
         </div>
       </div>
+    </div>
   );
 }
 
